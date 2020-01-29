e3df88928ef78c4fdc58668bc82a51a1d4938442
Petrasek, Nicholas <nicholas.petrasek@accenture.com>
2020-01-29 09:45:24 +0100

    Initial Commit

---
+++ modules/t3-rest-service/src/main/java/t3/rest/service/application/Message.java
@@ -0,0 +1,20 @@
+package t3.rest.service.application;
+
+public class Message {
+
+	private String message;
+	
+	public Message() {}
+	
+	public Message(String message) {
+		this.message = message;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+	
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
